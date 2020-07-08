866c05d455bc93c8e5fe88cebffbb0dd651a501d
khoivc <khoivc@novaon.asia>
2020-07-09 05:20:16 +0700

    uptime req

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const task = require("./CronTask");
+const express = require('express');
 
-task.start();
+const app = express();
+
+app.get('/uptime', (req, res) => {
+  console.log('uptime req');
+  res.send('oke');
+});
+
+app.listen(3000, (error) => {
+  if (error) {
+    console.error('ERROR - Unable to start app.');
+  } else {
+    console.info(`🚀 Server ready at http://localhost:3000/graphql`);
+  }
+});
+
+require('./CronTask').start();
