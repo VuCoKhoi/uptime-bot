84ebe8851517e5cbaa86eb77c843e97345f17721
khoivc <khoivc@novaon.asia>
2020-07-09 05:17:23 +0700

    add phantom

--- CronTask.js
+++ CronTask.js
@@ -1,19 +1,34 @@
-const cron = require("node-cron");
-const Http = require("./Http");
+const cron = require('node-cron');
+const phantom = require('phantom');
+const Http = require('./Http');
+
+const fakeBrowser = async (url) => {
+  const instance = await phantom.create();
+  const page = await instance.createPage();
+  await page.on('onResourceRequested', function (requestData) {
+    console.info('Requesting', requestData.url);
+  });
+
+  const status = await page.open(`${url}/uptime`);
+  const content = await page.property('content');
+  console.log(content);
+
+  await instance.exit();
+};
 
 const reqHandle = async () => {
-  process.env.URL.split(",").map((url) => Http.get(`${url}/uptime`));
+  process.env.URL.split(',').map(fakeBrowser);
 };
 
 const task = cron.schedule(
-  "*/5 * * * * *",
+  '*/2 * * * *',
   async () => {
-    console.log("running a task every two minutes");
+    console.log('running a task every two minutes');
     await reqHandle();
   },
   {
     scheduled: false,
-  }
+  },
 );
 
 module.exports = task;
